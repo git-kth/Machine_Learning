803005e865c7299143c868f4c7b1f95e28930a34
TaeHyun Kim <57697593+git-kth@users.noreply.github.com>
2020-07-02 20:51:15 +0900

    Update neuralnet_mnist.py

--- chap03/neuralnet_mnist.py
+++ chap03/neuralnet_mnist.py
@@ -35,15 +35,15 @@ x, t = get_data()
 network = init_network()
 
 batch_size = 100
-accurary_cnt = 0
+accuracy_cnt = 0
 
 for i in range(0,len(x),batch_size):
     x_batch = x[i:i + batch_size]
     y_batch = predict(network, x_batch)
     p = np.argmax(y_batch,axis = 1)
-    accurary_cnt += np.sum(p == t[i:i + batch_size])
+    accuracy_cnt += np.sum(p == t[i:i + batch_size])
 
-print("Accuray : " + str(float(accurary_cnt) / len(x)))
+print("Accuracy : " + str(float(accuracy_cnt) / len(x)))
     
     
 
